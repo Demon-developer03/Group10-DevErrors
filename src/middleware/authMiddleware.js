f764b3d7393445bcafc78bec2d430dec7e6accf7
unknown <johnreyambe02@gmail.com>
2026-09-22 18:39:56 +0800

    Added Middleware

--- src/middleware/authMiddleware.js
+++ src/middleware/authMiddleware.js
@@ -10,7 +10,7 @@ const authMiddleware = (req, res, next) => {
     const token = authHeader.split(' ')[1];
 
     try {
-        // Updated to match your exact setup
+        
         const decoded = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
         req.user = decoded;
         next();
